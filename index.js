397d56cb3939ae4ea070442d2242411b2fdbfdb8
cjalexander94 <cj.alexander94@gmail.com>
2017-04-11 20:55:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(){
+	//console.log("let's add");
+	var math = 7 + 7;
+	console.log(math)
+}
+
+function subtract(){
+	//console.log("let's subtract");
+	var math = 9 - 5;
+	console.log(math)
+}
+
+function multiply(){
+	//console.log("let's multiply");
+	var math = 3 * 3;
+	console.log(math)
+}
+
+function divide(){
+	//console.log("let's divide");
+	var math = 5/5;
+	console.log(math)
+}
